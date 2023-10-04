36ffc0a370d435085b4378982e36df1b95536f76
QueenMaggie <wanjirumargaret394@gmail.com>
2023-10-04 14:05:58 -0700

    updated

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+The N queens puzzle is the challenge of placing N
+non-attacking queens on an N×N chessboard.
+"""
+import sys
+
+
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
+
+
+def board_deepcopy(board):
+    """Return a deepcopy of a chessboard."""
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
+    return (board)
+
+
+def get_solution(board):
+    """Return the list of lists representation of a solved chessboard."""
+    solution = []
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if board[r][c] == "Q":
+                solution.append([r, c])
+                break
+    return (solution)
+
+
+def xout(board, row, col):
+    """X out spots on a chessboard.
+    Args:
+        board (list): The current working chessboard.
+        row (int): row
+        col (int): column
+    """
+    # X out all forward spots
+    for c in range(col + 1, len(board)):
+        board[row][c] = "x"
+    # X out all backwards spots
+    for c in range(col - 1, -1, -1):
---
+++ 0x08-python-more_classes/9-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+A class Rectangle that defines a rectangle by: (based on 7-rectangle.py)
+"""
+
+
+class Rectangle:
+    """
+    Represents class rectangle
+
+    Args:
+        width (int): width
+        height (int): height
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initialize rectangles"""
+        type(self).number_of_instances += 1
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Returns width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets returns height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
