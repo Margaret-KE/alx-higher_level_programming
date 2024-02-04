400cd8c4056f5fd425590e4a15641125ae636eaa
QueenMaggie <wanjiumargaret394@gmail.com>
2024-02-03 17:33:39 -0800

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A Python script that fetches https://alx-intranet.hbtn.io/status
+
+- Am using the package urllib
+- Am not allowed to import any packages other than urllib
+- The body of the response is displayed like the following
+        example (tabulation before -)
+"""
+
+
+from urllib import request
+
+
+if __name__ == "__main__":
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        response = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
